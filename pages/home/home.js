31c051a7ebdd4abe382f62f57f5524b2b927e373
xkcease <xkcease@163.com>
2021-02-18 15:33:38 +0800

    feat(pages): add home page

---
+++ pages/home/home.js
@@ -0,0 +1,34 @@
+// home.js
+// 获取应用实例
+const app = getApp();
+const { getHotelIntroRequest } = require('../../utils/request');
+
+Page({
+  data: {
+    hotelIntro: {},
+    roomIntro: {},
+  },
+  onLoad() {
+    wx.getStorage({
+      key: 'roomIntro',
+      success: (res) => {
+        this.setData({
+          roomIntro: res.data, 
+        })
+      }
+    })
+
+    getHotelIntroRequest().then(res => {
+      if (res.state) {
+        res.hotelIntro.optionsArray = res.hotelIntro.options.split(';');
+
+        this.setData({
+          hotelIntro: res.hotelIntro,
+        });
+      }
+      else {
+        console.log(res.msg);
+      }
+    });
+  },
+})
