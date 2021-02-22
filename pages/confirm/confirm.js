41ddebd283b5ce39f0541952306860789eea3952
xkcease <xkcease@163.com>
2021-02-22 19:02:16 +0800

    feat(pages): update confirm page

--- pages/confirm/confirm.js
+++ pages/confirm/confirm.js
@@ -1,13 +1,14 @@
 // pages/confirm/confirm.js
 const { getFormatDate, getFormatNextDate } = require('../../utils/dateTool');
 const { throttle } = require('../../utils/util');
+const { reserveRequest } = require('../../utils/request');
 
 Page({
   data: {
     date: {
       start: getFormatDate(Date.now()),
       end: getFormatNextDate(Date.now(), 1),
-      reservation_time: 0,
+      reservation_time: Date.now(),
       reservation_during: 1,
     },
     today: getFormatDate(Date.now()),
@@ -58,5 +59,32 @@ Page({
         total: this.data.number * this.data.room.price * this.data.date.reservation_during,
       });
     }, 80))();
-  }
+  },
+  reserve() {
+    let data = {
+      sessionId: wx.getStorageSync('sessionId'),
+      reservation_time: this.data.date.reservation_time,
+      reservation_during: this.data.date.reservation_during,
+      type: this.data.room.type,
+    };
+    
+    wx.showLoading({title: '加载中'});
+
+    reserveRequest(data).then(res => {
+      wx.hideLoading();
+
+      wx.showToast({
+        title: res.msg,
+        icon: res.state ? 'success' : 'error',
+        duration: 600
+      });
+    }).catch(err => {
+      console.log(err);
+      wx.showToast({
+        title: '失败',
+        icon: 'error',
+        duration: 600
+      });
+    })
+  },
 })
